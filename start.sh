3728ba259184509d369a17ffd57f60a6df1abc8b
Harshii03 <139242152+Harshii03@users.noreply.github.com>
2023-12-24 22:40:38 +0530

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,7 @@
+export BOT_TOKEN="6872984934:AAFzqPTx-1HWK-_3zl6djtFoIAxFHwTbV18"
+export API_ID=<api-id>
+export API_HASH="<api-hash>"
+export LOG_CHANNEL=<log-channel>
+export AUTH_USERS="2128674349 1894915577"
+export DB_URL="<mongodb-uri>"
+python3 main.py
