7bf7e113385d3bd803d311e982c14edd3e7b8b20
Harshii03 <139242152+Harshii03@users.noreply.github.com>
2023-12-29 21:33:52 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
-export BOT_TOKEN="6872984934:AAFzqPTx-1HWK-_3zl6djtFoIAxFHwTbV18"
+export BOT_TOKEN="6827546868:AAGpYbyBo2ixGZ4dFSftwljg2Z3cgmfZ_dg"
 export API_ID=<api-id>
 export API_HASH="<api-hash>"
 export LOG_CHANNEL=<log-channel>
