f95376c09725cdc157cd6edcbcf85e830db216af
Harshii03 <139242152+Harshii03@users.noreply.github.com>
2023-12-30 11:41:03 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 export BOT_TOKEN="6827546868:AAGpYbyBo2ixGZ4dFSftwljg2Z3cgmfZ_dg"
-export API_ID=<api-id>
-export API_HASH="<api-hash>"
-export LOG_CHANNEL=<log-channel>
-export AUTH_USERS="2128674349 1894915577"
-export DB_URL="<mongodb-uri>"
+export API_ID="23292000"
+export API_HASH="be15e99b531e98b996bd4518fffe162d"
+export LOG_CHANNEL="1002075695258"
+export AUTH_USERS="1758343727"
+export DB_URL="mongodb+srv://harshalamja:harshalamja@cluster0.mklu5od.mongodb.net/?retryWrites=true&w=majority"
 python3 main.py
